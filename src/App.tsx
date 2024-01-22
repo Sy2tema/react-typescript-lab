3979d3a36da2d6974b6fc53940671ab96f225a74
GunHyuk Lee <37396444+Sy2tema@users.noreply.github.com>
2024-01-23 00:38:37 +0900

    feat: 할일 추가 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 import { useState } from 'react';
 import { DataView } from 'components/DataView';
+import { TextInput } from 'components/TextInput';
+import { Button } from 'components/Button';
 
 const Container = styled.div`
   height: 100vh;
@@ -13,14 +15,24 @@ const Container = styled.div`
 
 function App() {
   const [toDoList, setToDoList] = useState(["리액트 공부하기", "타입스크립트 공부하기", "운동하기"]);
+  const [todo, setTodo] = useState("");
 
   const onDelete = (todo: string) => {
     setToDoList(toDoList.filter((item) => item !== todo));
   }
 
+  const onAddTodo = () => {
+    if (todo === '') return;
+
+    setToDoList([...toDoList, todo]);
+    setTodo("");
+  }
+
   return (
     <Container>
       <DataView toDoList={toDoList} onDelete={onDelete} />
+      <TextInput value={todo} onChange={setTodo} />
+      <Button label='추가' color='#304FFE' onClick={onAddTodo} />
     </Container>
   );
 }
